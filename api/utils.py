d8d49efe722b248a8fcba1bad2401e236c66d5c1
Al-Amen <alamen15@cse.pstu.ac.bd>
2025-08-24 11:32:44 +0600

    update media file

--- api/utils.py
+++ api/utils.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 from django.conf import settings
 
 def save_plot(plot_img_path):
+    os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
     image_path = os.path.join(settings.MEDIA_ROOT, plot_img_path)
+   
     plt.savefig(image_path)
     plt.close()
     plot_img = settings.MEDIA_URL + plot_img_path
--- railway-start.sh
+++ railway-start.sh
@@ -3,6 +3,6 @@ python manage.py migrate --noinput
 
 # Collect static files
 python manage.py collectstatic --noinput
-
+mkdir -p media
 # Start the app
 gunicorn stock_prediction_main.wsgi --log-file -
